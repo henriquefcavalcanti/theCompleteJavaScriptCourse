081fd106c41a782b8c9808a9dfb8f7d6725d7e2b
henriquefcavalcanti <henriquefcavalcanti@gmail.com>
2021-02-20 20:47:08 -0300

    data types

--- 01_jsFundamentals/script.js
+++ 01_jsFundamentals/script.js
@@ -1,15 +1,34 @@
-let js = 'amazing'
-console.log(40 + 8 + 23 - 10)
+// let js = 'amazing'
+// console.log(40 + 8 + 23 - 10)
 
-console.log('Jonas')
-console.log('23')
+// console.log('Jonas')
+// console.log('23')
 
-let firstName = 'Matilda'
-console.log(firstName)
-console.log(firstName)
+// let firstName = 'Matilda'
+// console.log(firstName)
+// console.log(firstName)
 
-const PI = 3.1415
+// const PI = 3.1415
 
-console.log(PI)
+// console.log(PI)
 
+let javaScriptIsFun = true
+console.log(javaScriptIsFun)
 
+// console.log(true)
+console.log(typeof javaScriptIsFun)
+// console.log(typeof 23)
+// console.log(typeof 'Jonas')
+
+javaScriptIsFun = 'YES!'
+console.log(typeof javaScriptIsFun)
+
+let year
+console.log(year)
+console.log(typeof year)
+
+year = 1991
+console.log(year)
+console.log(typeof year)
+
+console.log(typeof null)
