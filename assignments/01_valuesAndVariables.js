70469ed3a47437db946a6f36ed6021f896b2f400
henriquefcavalcanti <henriquefcavalcanti@gmail.com>
2021-02-20 17:47:22 -0300

    assignment 01

---
+++ assignments/01_valuesAndVariables.js
@@ -0,0 +1,7 @@
+let country = 'Brazil'
+let continent = 'South America'
+let population = 213.4
+
+console.log(`Country: ${country}`)
+console.log(`Continent: ${continent}`)
+console.log(`Population: ${population}`)
