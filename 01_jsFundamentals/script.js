003ce8b456054d37064fcfa3a4585b228a54ba19
henriquefcavalcanti <henriquefcavalcanti@gmail.com>
2021-02-20 17:36:06 -0300

    values and variables

--- 01_jsFundamentals/script.js
+++ 01_jsFundamentals/script.js
@@ -1,4 +1,15 @@
 let js = 'amazing'
-if (js === 'amazing') alert('JavaScript is FUN!')
-
 console.log(40 + 8 + 23 - 10)
+
+console.log('Jonas')
+console.log('23')
+
+let firstName = 'Matilda'
+console.log(firstName)
+console.log(firstName)
+
+const PI = 3.1415
+
+console.log(PI)
+
+
