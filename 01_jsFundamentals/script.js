619f1aefdcbac73da1986ea9868eb3f40974a289
henriquefcavalcanti <henriquefcavalcanti@gmail.com>
2021-02-20 20:59:39 -0300

    let, const and var

--- 01_jsFundamentals/script.js
+++ 01_jsFundamentals/script.js
@@ -12,23 +12,34 @@
 
 // console.log(PI)
 
-let javaScriptIsFun = true
-console.log(javaScriptIsFun)
+// let javaScriptIsFun = true
+// console.log(javaScriptIsFun)
 
 // console.log(true)
-console.log(typeof javaScriptIsFun)
+// console.log(typeof javaScriptIsFun)
 // console.log(typeof 23)
 // console.log(typeof 'Jonas')
 
-javaScriptIsFun = 'YES!'
-console.log(typeof javaScriptIsFun)
+// javaScriptIsFun = 'YES!'
+// console.log(typeof javaScriptIsFun)
 
-let year
-console.log(year)
-console.log(typeof year)
+// let year
+// console.log(year)
+// console.log(typeof year)
 
-year = 1991
-console.log(year)
-console.log(typeof year)
+// year = 1991
+// console.log(year)
+// console.log(typeof year)
 
-console.log(typeof null)
+// console.log(typeof null)
+
+let age = 30
+age = 31
+
+const birthYear = 1991
+// birthYear = 1990
+
+// const job
+
+var job = 'programmer'
+job = 'teacher'
