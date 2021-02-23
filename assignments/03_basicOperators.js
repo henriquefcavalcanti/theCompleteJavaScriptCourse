2e0c82996fc2ea607dc9e61f9dc837043f6e53b4
henriquefcavalcanti <henriquefcavalcanti@gmail.com>
2021-02-23 10:24:51 -0300

    assignment 3

---
+++ assignments/03_basicOperators.js
@@ -0,0 +1,12 @@
+let country = 'Brazil'
+let continent = 'South America'
+let population = 213.4
+let description = 'Brazil is in South America, and its 213.4 million people speak portuguese'
+
+console.log(population / 2)
+population++
+console.log(population)
+console.log(population > 6)
+console.log(population > 33)
+console.log(description)
+
