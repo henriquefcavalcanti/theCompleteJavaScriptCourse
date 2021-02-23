5060c411e0e9de5b65c720dd01028c2265ea9c87
henriquefcavalcanti <henriquefcavalcanti@gmail.com>
2021-02-23 10:04:07 -0300

    basic operators

--- 01_jsFundamentals/script.js
+++ 01_jsFundamentals/script.js
@@ -45,3 +45,31 @@
 // job = 'teacher'
 
 
+// Math operators
+const now = 2037
+const ageJonas = now - 2002
+const ageSarah = now - 2020
+console.log(ageJonas, ageSarah)
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3)
+// 2 ** means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Henrique'
+const LastName = 'Cavalcanti'
+console.log(firstName + ' ' + LastName)
+
+// Assignment operator
+let x = 10 + 5 // 10
+x += 10 // x = x + 10 = 25
+x *= 4 // x = x * 4 = 100
+x++ // x = x +1
+x-- // x = x - 1
+console.log(x)
+
+// Comparison operators
+console.log(ageJonas > ageSarah)
+console.log(ageSarah >= 18)
+
+const isFullAge = ageSarah >= 18
+
+console.log(now - 1991 > now - 2018)
