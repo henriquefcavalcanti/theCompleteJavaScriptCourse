9eaabf46bb3a9a07a0697dcea59eaa0f1da2d575
henriquefcavalcanti <henriquefcavalcanti@gmail.com>
2021-02-23 15:06:24 -0300

    coding challenge

--- 01_jsFundamentals/script.js
+++ 01_jsFundamentals/script.js
@@ -74,17 +74,33 @@
 
 // console.log(now - 1991 > now - 2018)
 
-const now = 2037
-const ageJonas = now - 1991
-const ageSarah = now - 2018
+// const now = 2037
+// const ageJonas = now - 1991
+// const ageSarah = now - 2018
 
-console.log(now - 1991 > now - 2018)
+// console.log(now - 1991 > now - 2018)
 
-console.log(25 - 10 - 5)
+// console.log(25 - 10 - 5)
 
-let x, y
-x = y = 25 - 10 - 5 // x = y = 10, x = 10
-console.log(x, y)
+// let x, y
+// x = y = 25 - 10 - 5 // x = y = 10, x = 10
+// console.log(x, y)
 
-const averageAge = (ageJonas + ageSarah) / 2
-console.log(ageJonas, ageSarah, averageAge)
+// const averageAge = (ageJonas + ageSarah) / 2
+// console.log(ageJonas, ageSarah, averageAge)
+
+
+
+
+//////////////////////
+// Coding Challenge #1
+const markHeight = 1.69
+const markMass = 78
+const johnHeight = 1.95
+const johnMass = 92
+
+const markBMI = markMass / markHeight ** 2
+const johnBMI = johnMass / johnHeight ** 2
+
+let markHigherBMI = markBMI > johnBMI
+console.log(markHigherBMI)
