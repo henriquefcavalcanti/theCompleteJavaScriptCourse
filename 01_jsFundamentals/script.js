c08faeaa981641a470ce30ed4478b366ddaec606
henriquefcavalcanti <henriquefcavalcanti@gmail.com>
2021-02-24 16:03:03 -0300

    truthy and falsy values

--- 01_jsFundamentals/script.js
+++ 01_jsFundamentals/script.js
@@ -162,22 +162,44 @@
 //     console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`)
 // }
 
-// type conversion
-const inputYear = '1991'
-console.log(Number(inputYear), inputYear)
-console.log(Number(inputYear) + 18)
+// // type conversion
+// const inputYear = '1991'
+// console.log(Number(inputYear), inputYear)
+// console.log(Number(inputYear) + 18)
 
-console.log(Number('Jonas'))
-console.log(typeof NaN)
+// console.log(Number('Jonas'))
+// console.log(typeof NaN)
 
-console.log(String(23), 23)
+// console.log(String(23), 23)
 
-// type coercion
-console.log('I am ' + 23 + ' years old')
-console.log('23' - '10' - 3)
-console.log('23' / '2')
-console.log('23' > '18')
+// // type coercion
+// console.log('I am ' + 23 + ' years old')
+// console.log('23' - '10' - 3)
+// console.log('23' / '2')
+// console.log('23' > '18')
 
-let n = '1' + 1
-n = n - 1
-console.log(n)
+// let n = '1' + 1
+// n = n - 1
+// console.log(n)
+
+// 5 falsy values: 0, '', undefined, null, NaN
+
+console.log(Boolean(0))
+console.log(Boolean(undefined))
+console.log(Boolean('Jonas'))
+console.log(Boolean({}))
+console.log(Boolean(''))
+
+const money = 100
+if(money) {
+    console.log("Don't spend it all!")
+} else {
+    console.log("You should get a job!")
+}
+
+let height = 81
+if(height) {
+    console.log('YAY! Height is defined')
+} else {
+    console.log('Height is UNDEFINED')
+}
