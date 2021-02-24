73c59cd834822649acc13716613cc8bfee8e7991
henriquefcavalcanti <henriquefcavalcanti@gmail.com>
2021-02-24 15:34:56 -0300

    taking decisions: if/else statemants

--- assignments/03_basicOperators.js
+++ assignments/03_basicOperators.js
@@ -10,3 +10,9 @@ console.log(population > 6)
 console.log(population > 33)
 console.log(description)
 
+
+if(population > 33) {
+    console.log(`${country}'s population is above average`)
+} else {
+    console.log(`${country}'s population is ${33 - population} million below average`)
+}
