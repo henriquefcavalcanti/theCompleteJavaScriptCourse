0ec845241e1dbe67147dd7ff1bb3581ee7ffa06e
henriquefcavalcanti <henriquefcavalcanti@gmail.com>
2021-02-26 20:37:08 -0300

    assignment: logical operators

--- assignments/05.js
+++ assignments/05.js
@@ -1,9 +1,19 @@
-const numNeighbors = Number(prompt('How many neighbor countries does your country have?'))
+// const numNeighbors = Number(prompt('How many neighbor countries does your country have?'))
 
-if (numNeighbors === 1) {
-    console.log('Only 1 border!')
-} else if (numNeighbors > 1) {
-    console.log('More than 1 border!')
+// if (numNeighbors === 1) {
+//     console.log('Only 1 border!')
+// } else if (numNeighbors > 1) {
+//     console.log('More than 1 border!')
+// } else {
+//     console.log('No borders!')
+// }
+
+const speakEnglish = false
+const lessThan50 = false
+const isIsland = false
+
+if (speakEnglish && lessThan50 && !isIsland) {
+    console.log('You should live in this country')
 } else {
-    console.log('No borders!')
+    console.log('This country does not meet your criteria')
 }
