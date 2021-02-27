882f6c4c0379dbea610d8feb96d9c14b9c65ce90
henriquefcavalcanti <henriquefcavalcanti@gmail.com>
2021-02-27 17:23:47 -0300

    assignment: the condicional (ternary) operator

--- assignments/05.js
+++ assignments/05.js
@@ -18,24 +18,29 @@
 //     console.log('This country does not meet your criteria')
 // }
 
-const language = 'spanish'
+// const language = 'spanish'
 
-switch (language) {
-    case 'chinese':
-        console.log('MOST number of native speakers!')
-        break
-    case 'spanish':
-        console.log('2nd place in number of native speakers')
-        break
-    case 'english':
-        console.log('3rd place')
-        break
-    case 'hindi':
-        console.log('Number 4')
-        break
-    case 'arabic':
-        console.log('5th most spoken language')
-        break
-    default:
-        console.log('Great language too')
-}
+// switch (language) {
+//     case 'chinese':
+//         console.log('MOST number of native speakers!')
+//         break
+//     case 'spanish':
+//         console.log('2nd place in number of native speakers')
+//         break
+//     case 'english':
+//         console.log('3rd place')
+//         break
+//     case 'hindi':
+//         console.log('Number 4')
+//         break
+//     case 'arabic':
+//         console.log('5th most spoken language')
+//         break
+//     default:
+//         console.log('Great language too')
+// }
+
+const population = 230
+
+population >= 33 ? console.log("Brazil's population is above average.") : console.log("Brazil's population is below average.")
+
