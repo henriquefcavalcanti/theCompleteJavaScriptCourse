6fb31143d34b19a0b19eae9af38ee018903b1a8c
henriquefcavalcanti <henriquefcavalcanti@gmail.com>
2021-02-27 17:18:41 -0300

    the conditional (ternary) operator

--- 01_jsFundamentals/script.js
+++ 01_jsFundamentals/script.js
@@ -301,13 +301,29 @@
 //     console.log('Not a valid day')
 // }
 
-3+4
-1991
-true && false && !false
+// 3+4
+// 1991
+// true && false && !false
 
-if (23 > 10) {
-    const str = '23 is bigger'
-} 
+// if (23 > 10) {
+//     const str = '23 is bigger'
+// } 
 
-const me = 'Henrique'
-console.log(`I'm ${2037 - 2002} years old ${me}`)
+// const me = 'Henrique'
+// console.log(`I'm ${2037 - 2002} years old ${me}`)
+
+const age = 23
+age >= 18 ? console.log('I like to drink wine') : console.log('I like to drink water')
+
+const drink = age >= 18 ? 'wine' : 'water'
+console.log(drink)
+
+let drink2
+if (age >= 18) {
+    drink2 = 'wine'
+} else {
+    drink2 = 'water'
+}
+console.log(drink2)
+
+console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`)
