38ff0523edcc6c80647eb32d9e393d05f2a1d30c
henriquefcavalcanti <henriquefcavalcanti@gmail.com>
2021-03-13 09:34:41 -0300

    functions

--- 02_jsFundamentals2/script.js
+++ 02_jsFundamentals2/script.js
@@ -1,11 +1,31 @@
-'use strict'
+// 'use strict'
 
-let hasDriversLicense = false
-const passTest = true
+// let hasDriversLicense = false
+// const passTest = true
 
-if(passTest) hasDriversLicense = true
-if(hasDriversLicense) console.log('I can drive')
+// if(passTest) hasDriversLicense = true
+// if(hasDriversLicense) console.log('I can drive')
 
-// let inteface = 'Audio'
-// const private = 534
+// // let inteface = 'Audio'
+// // const private = 534
 
+function logger() {
+    console.log('My name is Henrique!')
+}
+
+// calling / running / invoking the function
+logger()
+logger()
+logger()
+logger()
+logger()
+
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges)
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`
+    return juice
+}
+
+const appleJuice = fruitProcessor(0,5)
+console.log(appleJuice)
+console.log(fruitProcessor(0,5))
